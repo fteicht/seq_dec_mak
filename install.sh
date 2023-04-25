9bd25b831e5cd14027fa4d637596a7c3b9606399
Florent Teichteil <florent.teichteil@gmail.com>
2023-04-25 17:04:42 +0200

    Updated install.sh

--- install.sh
+++ install.sh
@@ -7,6 +7,8 @@ python -m pip install --upgrade pip
 python -m pip install ray\[rllib\]==2.2.0
 python -m pip install scikit-decide\[all\]
 python -m pip install pygame
+python -m pip install pddlgym==0.0.4
+python -m pip install pyperplan
 python -m pip install jupyterlab
 python -m pip install ipykernel
 python -m ipykernel install --user --name=seq_dec_mak
